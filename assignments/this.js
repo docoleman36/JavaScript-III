28382774089ebf65775fd21b1261bf9335812f5b
DJ Coleman <DJ@DJs-MacBook-Pro.local>
2019-05-27 16:34:11 -0700

    Defined the four principles of the this keyword

--- assignments/this.js
+++ assignments/this.js
@@ -1,13 +1,13 @@
 /* The for principles of "this";
-* in your own words. explain the four principle for the "this" keyword below.
-*
-* 1. 
-* 2. 
-* 3. 
-* 4. 
-*
-* write out a code example of each explanation above
-*/
+ * in your own words. explain the four principle for the "this" keyword below.
+ *
+ * 1. The first principle is the window/ global object binding. When calling "this" it is on the window object only.
+ * 2. Implicit Binding is used om function and is called by everything that is left of a preceding dot.
+ * 3. New binding is an object constructor that returns a object.
+ * 4. Explicit binding is when we use a method to explicitly bind the this keyword.
+ *
+ * write out a code example of each explanation above
+ */
 
 // Principle 1
 
