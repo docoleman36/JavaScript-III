22645c9ca09607d1965a04f1606b7e5c16af77d4
DJ Coleman <DJ@DJs-MacBook-Pro.local>
2019-05-27 20:45:18 -0700

    Completed this.js

--- assignments/this.js
+++ assignments/this.js
@@ -23,21 +23,53 @@ sayName("DJ");
 
 // code example for Implicit Binding
 
+const MyObject = function() {
+  this.name = "Bob";
+  this.myProperty = "property";
+};
+
+MyObject.prototype.doStuff = function(action) {
+  console.log(`${this.name} is ${action} !`);
+};
+
+var obj = new MyObject();
+
+obj.doStuff("awesome"); // prints 'MyObjectName is awesome!'
+
+// Principle 3
+
+// code example for New Binding
+
+function SouthPark(kid) {
+  this.name = "Kyle";
+  this.location = "SouthPark";
+  this.speak = function() {
+    console.log(`${this.name} lives in  ${this.location}`);
+  };
+}
+
+const eric = new SouthPark("Eric");
+const kenny = new SouthPark("Kenny");
+
+eric.speak();
+kenny.speak();
+
+// Principle 4
+
+// code example for Explicit Binding
+
 const MyObj = function() {
   this.name = "DJ";
   this.favColor = "Red";
 };
 
 MyObj.prototype.speak = function() {
-  return `My name is ${this.name} and my favorite color is ${this.favColor}`;
+  MyObj.call(this, name, favColor);
+  console.log(
+    `My name is ${this.name} and my favorite color is ${this.favColor}`
+  );
 };
 
-MyObj();
+var obj = new MyObj();
 
-// Principle 3
-
-// code example for New Binding
-
-// Principle 4
-
-// code example for Explicit Binding
+obj.speak();
